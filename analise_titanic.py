5e820c3d1ac69689f524b7fa004c8f0c1dda0894
juliao1288 <julioleandro832@gmail.com>
2026-06-28 09:48:11 -0300

    Adicionado função 4

--- analise_titanic.py
+++ analise_titanic.py
@@ -223,6 +223,63 @@ def distinct_values (file_body: list):
 
     print(f'\nExistem {distinct_values_num} valores distintos na coluna analisada.')
 
+#Função 4 - Taxa de Sobrevivência Geral e por Segmento
+
+def taxa_sobrevivencia(file_body: list):
+    """
+    Este procedimento recebe uma lista de listas, file_body, e faz verificações de taxa de sobrevivência.
+    Taxa Geral: Porcentagem total de sobreviventes em relação ao total de passageiros.
+    Por Sexo: Compara a taxa de sobrevivência entre male (homens) e female (mulheres)
+    Por Classe: Calcula qual a porcentagem de sobreviventes em cada Pclass (1ª, 2ª e 3ª classe).
+
+    Args:
+        file_body (list) = Lista de listas completa de todas as informações dos passageiros, sem o cabeçalho.
+
+
+    """
+    vivos_geral = 0
+    male = [0,0] #[total, sobreviventes]
+    female = [0,0] #[total, sobreviventes]
+    c_classes = [0,0,0]
+    vivos_classes = [0,0,0]
+
+
+    for linha in file_body:
+        #Contagem geral
+        if linha[1] == "1":
+            vivos_geral += 1
+
+        #Contagem homens e mulheres
+        if linha[4] == "male":
+            male[0] += 1
+            if linha[1] == "1":
+                male[1] += 1
+        elif linha[4] == "female":
+            female[0] += 1
+            if linha[1] == "1":
+                female[1] += 1
+                
+        #Contagem por classe
+        if linha[2] == "1":
+            c_classes[0] += 1
+            if linha[1] == "1":
+                vivos_classes[0] += 1
+        elif linha[2] == "2":
+            c_classes[1] += 1
+            if linha[1] == "1":
+                vivos_classes[1] += 1
+        elif linha[2] == "3":
+            c_classes[2] += 1
+            if linha[1] == "1":
+                vivos_classes[2] += 1
+
+    print(f" Na contagem geral, apenas {(vivos_geral/len(file_body)) * 100:.2f}% dos passageiros sobreviveram, o que corresponde a {vivos_geral} passageiros.\n" +
+          f" Dentre os {male[0]} homens, apenas {(male[1]/male[0]) * 100:.2f}% sobreviveram, o que corresponde a {male[1]} homens.\n" +
+          f" Dentre as {female[0]} mulheres, apenas {(female[1]/female[0]) *100:.2f}% sobreviveram, o que corresponde a {female[1]} mulheres.")
+    
+    for i in range(len(c_classes)):
+        print(f" Dentre os {c_classes[i]} da {i+1}° classe, apenas {(vivos_classes[i]/c_classes[i]) *100:.2f}% sobreviveram, o que corresponde a {vivos_classes[i]} passageiros.")
+
 #Atividade 5 Enzo
 def estatistica_familia (cleaned_file_body : list) :
     #Esta função analisa a 7º e 8º coluna da matriz (respectivamente, SibSp e Parch), calcula e printa quantas pessoas viajaram sozinho ou em grupo, além da média
